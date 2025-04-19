2f566633d4f22ce76d0785cc3279e04a5e818c61
Markus Staab <maggus.staab@googlemail.com>
2025-04-19 09:22:06 +0200

    disable xdebug in `nsrt`

--- phpstan/aliases.zsh
+++ phpstan/aliases.zsh
@@ -1,1 +1,1 @@
-alias nsrt="vendor/bin/phpunit tests/PHPStan/Analyser/NodeScopeResolverTest.php --cache-result --order-by=defects,random --stop-on-failure"
+alias nsrt="XDEBUG_MODE=off vendor/bin/phpunit tests/PHPStan/Analyser/NodeScopeResolverTest.php --cache-result --order-by=defects,random --stop-on-failure"
